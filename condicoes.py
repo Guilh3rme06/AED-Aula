6e7fc0db251bd875c5dfafe6249b0dfbc18150a9
Guilh3rme06 <164666171+Guilh3rme06@users.noreply.github.com>
2024-10-31 14:07:53 +0000

    condition

---
+++ condicoes.py
@@ -0,0 +1,28 @@
+#
+# Programa de exemplo para mostrar instruções condicionais
+#
+# Curso- Devenvolvimento para a web e dispositivos móveis
+#
+# UC- Algoritmos e Estrutura de Dados
+#
+# Nome- Guilherme Rodrigues
+#
+# Número- 22405041
+#
+# Data- 16/10/2024
+#
+#a < b - b maior que a
+#a > b - a maior que b
+#a =< b - b maior ou igual a a
+#< => b - a maior ou igual a b
+#a == b - a igual a b
+#a != b - a diferente de b
+#
+
+temperatura = 18
+
+if temperatura > 18:
+    print(f'A temperatura em º é: {temperatura} ou seja está calor')
+else:
+    print(f'A temperatura em º é: {temperatura} ou seja está frio')
+
