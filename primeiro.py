39540831fdaf1c7419ff5d476d055d74d4aeff7d
Guilh3rme06 <164666171+Guilh3rme06@users.noreply.github.com>
2024-10-31 14:02:35 +0000

    Introdução

---
+++ primeiro.py
@@ -0,0 +1,40 @@
+#
+# Primeira aula e primeiro projeto a trabalhar em py
+#
+# Curso- Devenvolvimento para a web e dispositivos móveis
+#
+# UC- Algoritmos e Estrutura de Dados
+#
+# Nome- Guilherme Rodrigues
+#
+# Número- 22405041
+#
+# Data- 09/10/2024
+#
+
+#Tipos de variaveis
+a = 22
+b= 44
+c= a+b/2
+d= 'array'
+e= 'e'
+f= 0.3
+
+print('Número 1:', a, 'Número 2:', b)
+
+print(a,'+', b, '=', a+b)
+print(a,'-', b, '=', a-b)
+print(a,'x', b, '=', a*b)
+print(a,'/', b, '=', a/b)
+print(a,'//', b, '=', a//b)
+print('reto de',a,'/', b, '=', a%b)
+print(a,'elevado', b, '=', a**b)
+
+#Outras formas de fazer
+#print(f"{a} + {b} = {a+b}")
+#print(f"{a} - {b} = {a-b}")
+#print(f"{a} x {b} = {a*b}")
+#print(f"{a} / {b} = {a/b}")
+#print(f"{a} // {b} = {a//b}")
+#print(f"O resto de {a} / {b} = {a/%b}")
+#print(f"{a} elevado a {b} = {a**b}")
